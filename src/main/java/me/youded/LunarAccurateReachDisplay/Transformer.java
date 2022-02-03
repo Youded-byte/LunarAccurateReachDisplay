b8b1a787b572c6661fc04deeec90eb02cb04325a
Youded-byte <82912625+Youded-byte@users.noreply.github.com>
2022-02-03 16:35:59 +0100

    Fix
    A poor way of finding the correct method was used, moved over to another way that should be more update-proof.

--- src/main/java/me/youded/LunarAccurateReachDisplay/Transformer.java
+++ src/main/java/me/youded/LunarAccurateReachDisplay/Transformer.java
@@ -8,37 +8,44 @@ import org.objectweb.asm.ClassWriter;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
 
 public class Transformer implements ClassFileTransformer {
-    private Boolean foundfunction = false;
-    
+
     @Override
-    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+            ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         if (classfileBuffer == null || classfileBuffer.length == 0) {
             return new byte[0];
         }
 
-        if(!className.startsWith("lunar/")){
+        if (!className.startsWith("lunar/")) {
             return classfileBuffer;
         }
 
         ClassReader cr = new ClassReader(classfileBuffer);
-        if( cr.getInterfaces().length == 0 && cr.getSuperName().startsWith("lunar/") ) {
+        if (cr.getInterfaces().length == 0 && cr.getSuperName().startsWith("lunar/")) {
             ClassNode cn = new ClassNode();
 
             cr.accept(cn, 0);
-            
+            Boolean hasString = false;
             for (MethodNode method : cn.methods) {
-                if (method.name.equals("text")) {
-                    if(!this.foundfunction){
-                        for(AbstractInsnNode insn : method.instructions) {
-                            if(insn.getOpcode() == Opcodes.LDC && (double)((LdcInsnNode)insn).cst == 3.0D) {
-                                method.instructions.set(insn, new LdcInsnNode(300.0D));
-                                this.foundfunction = true;
-                                ClassWriter cw = new ClassWriter(cr, 0);
-                                cn.accept(cw);
-                                return cw.toByteArray();
-                            }
+                Boolean foundstring = Arrays.stream(method.instructions.toArray())
+                        .filter(LdcInsnNode.class::isInstance)
+                        .map(LdcInsnNode.class::cast)
+                        .map(inst -> inst.cst)
+                        .anyMatch("[1.3 blocks]"::equals);
+                if(foundstring)
+                    hasString = true;
+            }
+            if (hasString) {
+                for (MethodNode method : cn.methods) {
+                    for (AbstractInsnNode insn : method.instructions) {
+                        if (insn.getOpcode() == Opcodes.LDC && ((LdcInsnNode)insn).cst.getClass() == Double.class && (Double) ((LdcInsnNode) insn).cst == 3.0D) {
+                            method.instructions.set(insn, new LdcInsnNode(300.0D));
+                            ClassWriter cw = new ClassWriter(cr, 0);
+                            cn.accept(cw);
+                            return cw.toByteArray();
                         }
                     }
                 }
