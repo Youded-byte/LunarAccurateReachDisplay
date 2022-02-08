451349bfa570b45be4826d6098ee3f629b03d4b8
Youded-byte <82912625+Youded-byte@users.noreply.github.com>
2022-02-08 14:35:24 +0100

    Add Ability to disable rounding of reach

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Lunar Client Accurate Reach Display
 
 This agent removes the cap of 3.0 reach in the reach display mod included in lunar client. This cap is unnecesarry and even misleading, as on rare occasions a hit can be landed at a slightly larger distance.
+This agent also removes the rounding of reach if the decimal ends with a zero, e.g. 0.00 being rounded to 0, as it can be unwanted for the size of the box to change frequently.
 
 ## Usage of this mod
 
 This agent can be used easily by using [lunar-client-qt](https://github.com/Youded-byte/lunar-client-qt).
+
+## Options
+You can pass the option/argument "round" in order for the agent **not** to change the rounding system.
--- src/main/java/me/youded/LunarAccurateReachDisplay/Transformer.java
+++ src/main/java/me/youded/LunarAccurateReachDisplay/Transformer.java
@@ -11,6 +11,12 @@ import java.security.ProtectionDomain;
 import java.util.Arrays;
 
 public class Transformer implements ClassFileTransformer {
+    private boolean round = false;
+
+    Transformer(String args){
+        if(args != null && !args.isBlank() && args.equals("round"))
+            this.round = true;
+    }
 
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
@@ -29,26 +35,37 @@ public class Transformer implements ClassFileTransformer {
 
             cr.accept(cn, 0);
             Boolean hasString = false;
+
             for (MethodNode method : cn.methods) {
                 Boolean foundstring = Arrays.stream(method.instructions.toArray())
                         .filter(LdcInsnNode.class::isInstance)
                         .map(LdcInsnNode.class::cast)
                         .map(inst -> inst.cst)
                         .anyMatch("[1.3 blocks]"::equals);
-                if(foundstring)
+
+                if (foundstring)
                     hasString = true;
             }
             if (hasString) {
                 for (MethodNode method : cn.methods) {
+                    if (method.name.equals("<clinit>") && method.desc.equals("()V") && !this.round)
+                        for (AbstractInsnNode insn : method.instructions) {
+                            if (insn.getOpcode() == Opcodes.LDC && ((LdcInsnNode) insn).cst.getClass() == String.class
+                                    && ((String) ((LdcInsnNode) insn).cst).equals("#.##")) {
+                                method.instructions.set(insn, new LdcInsnNode("0.00"));
+                            }
+                        }
+
                     for (AbstractInsnNode insn : method.instructions) {
-                        if (insn.getOpcode() == Opcodes.LDC && ((LdcInsnNode)insn).cst.getClass() == Double.class && (Double) ((LdcInsnNode) insn).cst == 3.0D) {
-                            method.instructions.set(insn, new LdcInsnNode(300.0D));
-                            ClassWriter cw = new ClassWriter(cr, 0);
-                            cn.accept(cw);
-                            return cw.toByteArray();
+                        if (insn.getOpcode() == Opcodes.LDC && ((LdcInsnNode) insn).cst.getClass() == Double.class
+                                && (Double) ((LdcInsnNode) insn).cst == 3.0D) {
+                            method.instructions.set(insn, new LdcInsnNode(300.00D));
                         }
                     }
                 }
+                ClassWriter cw = new ClassWriter(cr, 0);
+                cn.accept(cw);
+                return cw.toByteArray();
             }
         }
         return classfileBuffer;
